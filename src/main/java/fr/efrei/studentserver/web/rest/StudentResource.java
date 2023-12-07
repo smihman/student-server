7c34107010b142885ca0a38fcb750588963654ca
smihman <smihman@gmail.com>
2023-12-07 17:28:46 +0100

    Change Java version

--- src/main/java/fr/efrei/studentserver/web/rest/StudentResource.java
+++ src/main/java/fr/efrei/studentserver/web/rest/StudentResource.java
@@ -1,2 +1,14 @@
-package fr.efrei.studentserver.web.rest;public class studentResource {
+package fr.efrei.studentserver.web.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class StudentResource {
+    @GetMapping("/student")
+    public String test(){
+        return "Hello World !";
+    }
 }
